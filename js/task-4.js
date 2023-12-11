11c88be7d5b5bf018624c5b8c1b581753e5364a8
Roman Fedevych <fedevychr@gmail.com>
2023-12-11 23:42:49 +0200

    task-4

---
+++ js/task-4.js
@@ -0,0 +1,208 @@
+// Метод filter()
+
+// Доповни код так, щоб у змінній evenNumbers був масив парних чисел із масиву numbers, а у змінній oddNumbers — масив непарних. Обов'язково використовуй метод filter().
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+// console.log(evenNumbers); // [24, 82, 36, 18, 52]
+// console.log(oddNumbers); // [17, 61, 47, 73]
+
+//////////////////////////////////////////
+
+// Масив books містить колекцію об'єктів книг, кожен з яких містить властивості title, author, rating. Використовуючи метод filter(), доповни код таким чином, щоб:
+
+// У змінній topRatedBooks утворився масив книг, рейтинг яких (властивість rating) більший за або дорівнює значенню змінної MIN_RATING.
+// У змінній booksByAuthor утворився масив книг, написаних автором з ім'ям (властивість author), яке збігається зі значенням у змінній AUTHOR.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+
+// const topRatedBooks = books.filter(book => book.rating >= 8);
+// const booksByAuthor = books.filter(book => book.author === 'Bernard Cornwell');
+
+// console.log(topRatedBooks); // це масив книг з рейтингом, вищим за `8`
+// console.log(booksByAuthor); // це масив книг, автор яких `"Bernard Cornwell"`
+
+/////////////////////////////////////////
+
+// Доповни функцію getUsersWithEyeColor(users, color) таким чином, щоб вона повертала масив користувачів, у яких колір очей (властивість eyeColor) збігається зі значенням другого параметра color.
+
+// const getUsersWithEyeColor = (users, color) => {
+//     const usersEyeColor = users.filter(user => user.eyeColor === color);
+//     return usersEyeColor;
+// };
+
+
+
+// const users = [
+//     {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//     age: 37
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//     age: 34
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//     age: 24
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//     age: 21
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//     age: 27
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//     age: 38
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//     age: 39
+//   }
+// ];
+
+// console.log(getUsersWithEyeColor(users, 'green')); // `Ross Vazquez` і `Elma Head`
+// console.log(getUsersWithEyeColor(users, 'color')); // []
+// console.log(getUsersWithEyeColor(users, 'brown')); // `Blackburn Dotson` і `Sheree Anthony`
+// console.log(getUsersWithEyeColor(users, 'blue')); // `Moore Hensley`, `Sharlene Bush` і `Carey Barr`
+
+/////////////////////////////////
+
+// Доповни код функції getUsersWithAge(users, minAge, maxAge), щоб повернути масив користувачів, вік (збережений у властивості age) яких потрапляє у заданий діапазон від minAge до maxAge.
+
+// Поради:
+
+// Використай метод filter() для створення нового масиву, в якому зберігаються тільки елементи, які задовольняють певну умову.
+// Використай оператори >= (більше або дорівнює) та <= (менше або дорівнює), щоб відфільтрувати користувачів, вік яких точно потрапляє у діапазон між мінімальним minAge та максимальним maxAge значеннями.
+
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//     const usersWithAge = users.filter(
+//         ({ age }) => age >= minAge && age <= maxAge
+//     );
+//     return usersWithAge;
+// }
+
+// console.log(getUsersWithAge(users, 20, 30)); // Ross Vazquez, Elma Head і Carey Barr
+// console.log(getUsersWithAge(users, 30, 40)); // Moore Hensley, Sharlene Bush, Blackburn Dotson, Sheree Anthony
+// console.log(getUsersWithAge(users, 80, 100)); // []
+
+////////////////////////////////////////////////
+
+// Метод find()
+
+// Масив books містить колекцію об'єктів книг, кожен з яких містить властивості title, author, rating.
+
+// Використовуючи метод find(), доповни код таким чином, щоб:
+
+// У змінній bookWithTitle утворився об'єкт книги, назва якої (властивість title) збігається зі значенням змінної BOOK_TITLE.
+// У змінній bookByAuthor утворився об'єкт книги, автор якої (властивість author) збігається зі значенням змінної AUTHOR.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+// ];
+// const BOOK_TITLE = "The Dream of a Ridiculous Man";
+// const AUTHOR = "Robert Sheckley";
+
+// const bookWithTitle = books.find((book) => book.title === "The Dream of a Ridiculous Man");
+// const bookByAuthor = books.find((book) => book.author === "Robert Sheckley");
+
+// console.log(bookWithTitle); // це об'єкт книги з назвою "The Dream of a Ridiculous Man"
+// console.log(bookByAuthor); // це об'єкт книги автора "Robert Sheckley"
+
+/////////////////////////////////
+
+// Доповни функцію getUserWithEmail(users, email) таким чином, щоб вона повертала об'єкт користувача, пошта якого (властивість email) збігається зі значенням другого параметра email.
+
+// const getUserWithEmail = (users, email) => {
+//     const userWithEmail = users.find((user) => user.email === email);
+//     return userWithEmail;
+// };
+
+// console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {Sheree Anthony}
+// console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {Elma Head}
+// console.log(getUserWithEmail(users, 'blackburndotson@furnigeer.com')); // {Blackburn Dotson}
+// console.log(getUserWithEmail(users, 'email')); // undefined
